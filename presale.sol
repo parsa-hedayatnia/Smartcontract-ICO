b724f45da1910ef897394993b39d874b97da45c2
parsa-hedayatnia <95084133+parsa-hedayatnia@users.noreply.github.com>
2023-08-04 16:57:55 +0330

    import openzeppelin lib

--- presale.sol
+++ presale.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.7.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 contract Presale{
-    
+
 }
