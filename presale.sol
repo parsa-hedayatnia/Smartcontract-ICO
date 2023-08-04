2bbc69633ac9db23c3f618a3bc389153d52286e0
parsa-hedayatnia <95084133+parsa-hedayatnia@users.noreply.github.com>
2023-08-04 18:31:02 +0330

    defined state variables

--- presale.sol
+++ presale.sol
@@ -1,12 +1,32 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.7.0;
+// SPDX-License-Identifier: FUM
+pragma solidity >= 0.7.0 <= 0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
-contract Presale{
+
+contract Presale is ReentrancyGuard, Context, Ownable {
+    mapping(address => uint) public _contributions;
+
+    IERC20 public _token;
+    uint private _tokendecimals;
+    address payable public _wallet;
+    uint public _rate;
+    uint public _weiRaised;
+    uint public endICO;
+    uint public availableTokenICO;
+    event TokenPurchased(address purchaser, address beneficiary, uint value, uint amount);
+    event Refund(address receipient, uint amount);
+
+    modifier icoActive() {
+        require(endICO > 0 && block.timestamp < endICO && availableTokensICO > 0, "ICO must be active");
+        _;
+    }
+    modifier icoNotActive() {
+        require(endICO < block.timestamp, "ICO should not be active");
+        _;
+    }
 
 }
